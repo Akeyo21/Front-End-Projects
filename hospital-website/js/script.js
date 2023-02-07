cfff695c4036d0fa16048d4e62c447c8ea3a3d3c
Akeyo21 <vickyakeke@gmail.com>
2023-02-07 08:02:50 +0300

    Adding js to handle showing and hiding of the navbar

--- hospital-website/js/script.js
+++ hospital-website/js/script.js
@@ -3,4 +3,10 @@ let navbar = document.querySelector("#navigation");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
 };
